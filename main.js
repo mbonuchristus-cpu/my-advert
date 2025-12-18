f9396d2b6a9d373c78c42a2835666086ab071813
mbonuchristus-cpu <mbonuchristus@gmail.com>
2025-12-18 21:20:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,4 +22,22 @@ const revealOnScroll = () => {
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
 
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(form),
+    headers: { 'Accept': 'application/json' }
+  })
+  .then(response => {
+    if(response.ok){
+      alert("Message sent! I will respond shortly.");
+      form.reset();
+    } else {
+      alert("Oops! Something went wrong.");
+    }
+  });
+});
 
